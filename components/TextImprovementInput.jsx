e32807cb6c93c9e12e6dfb9b695464ba98247f93
Flafi87 <laszlo.gaal@newnative.ai>
2023-06-26 11:49:12 +0200

    textbox fix

--- components/TextImprovementInput.jsx
+++ components/TextImprovementInput.jsx
@@ -99,13 +99,25 @@ const TextImprovementInput = () => {
           display: flex;
           flex-direction: column;
           width: 70%;
+          margin: 0 auto;
+          height: 350px;
         }
         #textbox {
           margin-bottom: 10px;
+          width: 100%;
+          min-width: 100%;
+          max-width: 100%;
+          height: 70%;
+          resize: none;
         }
-        .notthebest {
-          background-color: #ff0000;
-          font-color: red;
+        button {
+          width: 100px;
+          height: 30px;
+          background-color: #4caf50;
+          color: white;
+          border: none;
+          border-radius: 4px;
+          cursor: pointer;
         }
       `}</style>
     </>
